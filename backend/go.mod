cfaf5c5aacf90b19c3de8ffaa815f042580e5652
athryzorian <ravi.teli@thryzor.com>
2025-09-07 21:01:19 +0530

    UI and backend changes together

--- backend/go.mod
+++ backend/go.mod
@@ -1,4 +1,4 @@
-go 1.23
+go 1.23.0
 
 toolchain go1.24.6
 
@@ -11,6 +11,7 @@ require (
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
@@ -20,6 +21,7 @@ require (
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 
 	"github.com/cenkalti/backoff/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
+)
+
+// Scopes: OAuth 2.0 scopes provide a way to limit the amount of access that is granted to an access token.
+var googleOauthConfig = &oauth2.Config{
+	RedirectURL:  "http://localhost:8000/auth/google/callback",
+	ClientID:     "171559914291-q2rsnsoeae3u90ptc7hn3pe6hud7aru2.apps.googleusercontent.com",
+	ClientSecret: "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",
+	Scopes: []string{
+		"https://www.googleapis.com/auth/userinfo.email",
+	},
+	Endpoint: google.Endpoint,
+}
+
+type Message struct {
+	Value string `json:"value"`
+}
+
+const (
+	oauthGoogleUrlAPI = "https://accounts.google.com/o/oauth2/v2/auth"
 )
 
 func main() {
@@ -30,6 +56,18 @@ func main() {
 		return rootHandler(db, c)
 	})
 
+	e.GET("/auth/google/login", func(c echo.Context) error {
+		return oauthGoogleLogin(c.Response().Writer, c.Request())
+	})
+
+	e.POST("/login", func(c echo.Context) error {
+		return loginHandler(c)
+	})
+
+	e.GET("/auth/google/callback", func(c echo.Context) error {
+		return oauthGoogleCallback(c.Response().Writer, c.Request())
+	})
+
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
@@ -46,8 +84,69 @@ func main() {
 	e.Logger.Fatal(e.Start(":" + httpPort))
 }
 
-type Message struct {
-	Value string `json:"value"`
+func loginHandler(c echo.Context) error {
+	fmt.Println("Login Handler")
+	return c.HTML(http.StatusOK, `<html><body><a href="/auth/google/login">Backed received login call after successful Google Login.</a></body></html>`)
+}
+
+func oauthGoogleLogin(w http.ResponseWriter, r *http.Request) error {
+
+	fmt.Println("Google Login")
+
+	oauthState := generateStateOauthCookie(w)
+	u := googleOauthConfig.AuthCodeURL(oauthState, oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
+
+	return nil
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+
+	return state
+}
+
+func oauthGoogleCallback(w http.ResponseWriter, r *http.Request) error {
+
+	fmt.Println("Auth Google Callback")
+
+	data, err := getUserDataFromGoogle(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return nil
+	}
+
+	fmt.Fprintf(w, "UserInfo: %s\n", data)
+
+	return nil
+}
+
+func getUserDataFromGoogle(code string) ([]byte, error) {
+
+	// Use code to get token and get user info from Google.
+	token, err := googleOauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
+	}
+
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+	defer response.Body.Close()
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
+	}
+
+	//saveUser(contents)
+	//saveToken(contents, token)
+	return contents, nil
 }
 
 func initStore() (*sql.DB, error) {
@@ -93,6 +192,7 @@ func initStore() (*sql.DB, error) {
 }
 
 func rootHandler(db *sql.DB, c echo.Context) error {
+
 	r, err := countRecords(db)
 	if err != nil {
 		return c.HTML(http.StatusInternalServerError, err.Error())
